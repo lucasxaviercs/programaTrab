8335505837fa2b9b91c48d3d9e87bc22461ff24b
lucasxavier <lxavierfla@gmail.com>
2026-03-24 21:01:48 -0300

    correction

--- programaTrab.c
+++ programaTrab.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    int funcionalidade = atoi(argv[0]);
+    int funcionalidade = atoi(argv[1]);
     switch (funcionalidade) {
         case 1:
             printf("Funcionalidade 1 selecionada.\n");
